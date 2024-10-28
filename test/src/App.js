2cc010a2ca0584c0a070845151e624e15c7bce48
projectuser-gl <emreyilmazya@gmail.com>
2024-10-28 18:04:15 +0300

    first commit

---
+++ test/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="bg-blue-500 text-white">Tailwind çalışıyor!</div>
+
+      
+  );
+}
+
+export default App;
---
+++ test/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Tailwind sınıflarını içeren dosya yollarını burada belirtin
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
